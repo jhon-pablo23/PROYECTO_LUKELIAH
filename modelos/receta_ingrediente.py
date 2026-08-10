24d4677a838a139368e3960901917c2db82215ab
PCuaquira <patricia.cuaquiraa@istpargentina.edu.pe>
2026-08-09 21:56:22 -0500

    actualizar archivo receta_ingrediente.py y agregar metodo to_dict

--- modelos/receta_ingrediente.py
+++ modelos/receta_ingrediente.py
@@ -1,13 +1,12 @@
 class RecetaIngrediente:
-
     def __init__(self, id_receta, id_ingrediente, cantidad):
         self.id_receta = id_receta
         self.id_ingrediente = id_ingrediente
         self.cantidad = cantidad
-
-    def __str__(self):
-        return (
-            f"Receta: {self.id_receta} | "
-            f"Ingrediente: {self.id_ingrediente} | "
-            f"Cantidad: {self.cantidad}"
-        )
+        
+def to_dict(self):
+        return {
+            "id_receta": self.id_postre,
+            "id_ingrediente": self.id_receta,
+            "cantidad":self.cantidad,
+        }
