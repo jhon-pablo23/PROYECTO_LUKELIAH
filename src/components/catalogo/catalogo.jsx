fad2556f9e602fa94a83eee633c3327d82d0716e
jhon-pablo23 <jhon.anchayhuap@istpargentina.edu.pe>
2026-08-10 16:02:27 -0500

    frontend se corrigio el boton de whatsapp

--- src/components/catalogo/catalogo.jsx
+++ src/components/catalogo/catalogo.jsx
@@ -26,19 +26,36 @@ function Catalogo() {
     ];
 
     return (
-        <div className="catalogo-container">
 
-            {/* TITULO */}
+        <div className="catalogo">
 
-            <div className="catalogo-header">
+            {/* ENCABEZADO */}
 
-                <h1>
-                    Catálogo Público
-                </h1>
+            <div className="catalogo-header d-flex justify-content-between align-items-center">
 
-                <p>
-                    Conoce nuestros postres disponibles
-                </p>
+                <div>
+
+                    <h1>
+                        Catálogo Público
+                    </h1>
+
+                    <p>
+                        Conoce nuestros postres disponibles
+                    </p>
+
+                </div>
+
+
+                {/* BOTON DE WHATSAPP */}
+
+                <a
+                    href="https://wa.me/51912560956"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn btn-success"
+                >
+                    WhatsApp
+                </a>
 
             </div>
 
@@ -56,8 +73,6 @@ function Catalogo() {
 
                         <div className="catalogo-card">
 
-                            {/* IMAGEN */}
-
                             <div className="catalogo-imagen">
 
                                 <span>
@@ -65,9 +80,6 @@ function Catalogo() {
                                 </span>
 
                             </div>
-
-
-                            {/* INFORMACION */}
 
                             <div className="catalogo-info">
 
