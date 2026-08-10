aac640233a5eb3a72098d1d163bf2d0d2b094b37
jhon-pablo23 <jhon.anchayhuap@istpargentina.edu.pe>
2026-08-10 11:11:54 -0500

    se creo archivos de react y boostrap para el frontend

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">LUKELIAH</h1>
+
+      <div className="card mt-4">
+        <div className="card-body text-center">
+          <h3>Frontend funcionando</h3>
+          <p>React y Bootstrap están funcionando correctamente.</p>
+
+          <button className="btn btn-primary">
+            LUKELIAH
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
