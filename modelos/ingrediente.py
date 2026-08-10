7f1d66bf7a598ab0330e1bfd5d651a04e98fc4fe
PCuaquira <patricia.cuaquiraa@istpargentina.edu.pe>
2026-08-09 21:53:45 -0500

    actualizar archivo ingrediente.py y agregar metodo to_dict

--- modelos/ingrediente.py
+++ modelos/ingrediente.py
@@ -1,17 +1,18 @@
 class Ingrediente:
-
-    def __init__(self, nombre, unidad_medida, stock_actual, stock_minimo, costo):
-        self.id_ingrediente = None
+    def __init__(self, nombre, unidad_medida, stock_actual, stock_minimo, costo, id_ingrediente=None):
+        self.id_ingrediente = id_ingrediente
         self.nombre = nombre
         self.unidad_medida = unidad_medida
         self.stock_actual = stock_actual
         self.stock_minimo = stock_minimo
         self.costo = costo
 
-    def __str__(self):
-        return (
-            f"[{self.id_ingrediente}] "
-            f"{self.nombre} | "
-            f"{self.stock_actual} {self.unidad_medida} | "
-            f"S/ {self.costo:.2f}"
-        )
+    def to_dict(self):
+        return {
+            "id_ingrediente": self.id_ingrediente,
+            "nombre": self.nombre,
+            "unidad_medida": self.unidad_medida,
+            "stock_actual": self.stock_actual,
+            "stock_minimo": self.stock_minimo,
+            "costo": self.costo,
+        }
