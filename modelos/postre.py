f8e93e1d4d127bd21717f85560cf7b3d6c4cd09f
PCuaquira <patricia.cuaquiraa@istpargentina.edu.pe>
2026-08-09 21:54:48 -0500

    actualizar archivo postre.py y agregar metodo to_dict

--- modelos/postre.py
+++ modelos/postre.py
@@ -1,15 +1,16 @@
 class Postre:
-
-    def __init__(self, id_receta, nombre, precio, imagen):
-        self.id_postre = None
+    def __init__(self, nombre, precio, id_receta=None, imagen=None, id_postre=None):
+        self.id_postre = id_postre
         self.id_receta = id_receta
         self.nombre = nombre
         self.precio = precio
         self.imagen = imagen
 
-    def __str__(self):
-        return (
-            f"[{self.id_postre}] "
-            f"{self.nombre} | "
-            f"S/ {self.precio:.2f}"
-        )
+    def to_dict(self):
+        return {
+            "id_postre": self.id_postre,
+            "id_receta": self.id_receta,
+            "nombre": self.nombre,
+            "precio": self.precio,
+            "imagen": self.imagen,
+        }
